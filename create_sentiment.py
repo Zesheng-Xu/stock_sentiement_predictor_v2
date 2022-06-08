b55e7b834e0ee7a408ffa413e064f9332e7fcd2b
MDSJ.A <43301679+8SK3PS8@users.noreply.github.com>
2022-06-08 18:34:53 -0500

    Added Sentiment Code
    This will probably work. It needs to be tested as I haven't seen my code in around a year

--- create_sentiment.py
+++ create_sentiment.py
@@ -1,1 +1,10 @@
+from textblob import TextBlob
+import nltk
+from textblob.sentiments import NaiveBayesAnalyzer
+import tweepy
+import numpy
 
+
+def returnComputedSentiment(inputString):
+  opinion = TextBlob(inputString, analyzer=NaiveBayesAnalyzer())
+  return opinion.sentiment
